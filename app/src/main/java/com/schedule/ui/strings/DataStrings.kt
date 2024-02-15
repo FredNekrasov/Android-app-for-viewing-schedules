e4d1cd2dcfaac8406227fb4609a8e551e38c9320
FredNekrasov <frednekrasov49@gmail.com>
2024-02-15 03:19:32 +0300

    creating data strings for testing and sending error messages

---
+++ app/src/main/java/com/schedule/ui/strings/DataStrings.kt
@@ -0,0 +1,52 @@
+package com.schedule.ui.strings
+
+object DataStrings {
+    const val profile = "Profile"
+    const val teacherTitle = "Teachers"
+    const val subjectTitle = "Subjects"
+    const val groupTitle = "Groups"
+    const val audienceTypeTitle = "Audience Types"
+    const val audienceTitle = "Audiences"
+    const val pairTitle = "Pairs"
+    const val semesterTitle = "Semesters"
+    const val weekTitle = "Weeks"
+    const val dayTitle = "Days"
+
+    //user
+    const val userNameT = "User Name"
+    const val userPassword = "Password"
+    const val userEmail = "E-mail"
+
+    //teacher
+    const val teacherName = "Name"
+    const val teacherSurname = "Surname"
+    const val teacherPatronymic = "Patronymic"
+
+    //subject
+    const val lectureHours = "Lecture Hours"
+    const val practiceHours = "Practice Hours"
+    const val totalHours = "Total Hours"
+    const val consultationHours = "Consultation Hours"
+    const val typeOfCertificate = "Type of Certificate"
+
+    //group
+    const val groupNumber = "Group Number"
+    const val shortGroupNumber = "Short Group Number"
+    const val numberOfStudents = "Number of Students"
+
+    //audienceType
+    const val typeName = "Type Name"
+    const val description = "Description"
+
+    //audience
+    const val audienceNumber = "Audience Number"
+    const val audienceType = "Audience Type"
+    const val seatsNumber = "Seats Number"
+    const val seatsForStudents = "Seats For Students"
+
+    //semester
+    const val semesterNumber = "Semester Number"
+
+    //week
+    const val weekNumber = "Week Number"
+}
---
+++ app/src/main/java/com/schedule/ui/strings/InternetStrings.kt
@@ -0,0 +1,13 @@
+package com.schedule.ui.strings
+
+object InternetStrings {
+    const val success = "The data was uploaded successfully"
+    const val error = "Error"
+    const val noData = "No data"
+    const val loading = "Loading..."
+    const val unknown = "Unknown $error"
+    const val connectionError = "$error: There is no connection to the server"
+    const val noInternet = "$error: No internet connection"
+    const val jsonConversionError = "Json conversion error"
+    const val locationError = "Couldn't retrieve location. Make sure to grant permission and enable GPS."
+}
