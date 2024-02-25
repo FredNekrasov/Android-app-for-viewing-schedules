13b1dafb61217a72ba3d7d8fe1db0955714dccd2
FredNekrasov <frednekrasov49@gmail.com>
2024-02-25 12:41:47 +0300

    creating a file for qualifier names

---
+++ app/src/main/java/com/schedule/ui/strings/KoinDIQualifierStrings.kt
@@ -0,0 +1,18 @@
+package com.schedule.ui.strings
+
+object KoinDIQualifierStrings {
+    const val service = "Service"
+    const val repository = "Repository"
+    const val useCase = "UseCase"
+    const val vm = "ViewModel"
+
+    const val userEntity = "User"
+    const val teacherEntity = "Teacher"
+    const val subjectEntity = "Subject"
+    const val squadEntity = "Squad"
+    const val audienceTypeEntity = "AudienceType"
+    const val audienceEntity = "Audience"
+    const val semesterEntity = "Semester"
+    const val weekEntity = "Week"
+    const val pairEntity = "Pair"
+}
